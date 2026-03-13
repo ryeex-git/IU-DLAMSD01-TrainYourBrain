62baa1dd34ab1f1f62743f38bdd38e37b2e75254
dominikbayer <bayer.dominik.1405@gmail.com>
2026-03-13 18:51:39 +0100

    Initial Commit

---
+++ TrainYourBrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainYourBrain
+//
+//  Created by Dominik on 13.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainYourBrain/TrainYourBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainYourBrainApp.swift
+//  TrainYourBrain
+//
+//  Created by Dominik on 13.03.26.
+//
+
+import SwiftUI
+
+@main
+struct TrainYourBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
